886561e63ca03ebfc1a9e92c0652240c7ac19cf8
koosala <nayak.praveen@gmail.com>
2015-08-29 18:13:30 +0530

    First version of map viewer

---
+++ MapDisplay/Controllers/Map.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MapDisplay.Controllers
+{
+    public class MapController : Controller
+    {
+        //
+        // GET: /Map/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Population()
+        {
+            return View();
+        }
+    }
+}
---
+++ MapDisplay/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MapDisplay.MvcApplication" Language="C#" %>
